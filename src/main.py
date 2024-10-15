851314ba547dd4fae968b40327d185b56b9cb04b
mittaan <andremitta@gmail.com>
2024-10-15 09:41:12 +0200

    Added logic to get ids

--- src/main.py
+++ src/main.py
@@ -1,10 +1,39 @@
 #!user/bin/env python3
 
 import requests
+import yaml
+from pathlib import Path
 
-URL = "https://graph.instagram.com/v1/shortcode/<SHORTCODE>"
+TEMPLATE_URL = "https://graph.instagram.com/"
 
-PARAMS = {'fields': ['id', 'media_product_type'], 'access_token': 1}
+TAG_ID_URL = TEMPLATE_URL + "ig_hashtag_search?user_id=<USER_ID>&q=cats"
+
+TOP_MEDIA_URL = TEMPLATE_URL + "<TAG_ID>/top_media?user_id=<USER_ID>"
+
+def get_tag_id(user_id):
+    URL = TAG_ID_URL.replace("<USER_ID>", str(user_id))
+    r = requests.get(URL)
+    data = r.json()
+    return data["id"]
+
+def get_top_media_id(tag_id, user_id):
+    URL = TOP_MEDIA_URL.replace("<USER_ID>", str(user_id)).replace("<TAG_ID>", str(tag_id))
+    r = requests.get(URL)
+    data = r.json()
+    return data
+
+def main():
+    path = Path("../config/config.yml")
+    with open(path, "r") as config_file:
+        config = yaml.safe_load(config_file)
+
+    creadentials = config["credentials"]
+    user_id = creadentials["user_id"]
+    tag_id = get_tag_id(user_id)
+
+    data = get_top_media_id(tag_id, user_id)
+    print(data)
+
 
 if __name__ == "__main__":
-    pass
+    main()
