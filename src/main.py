117ce1a81ebb9f04ba5c3c300db3896d90c7239d
mittaan <andremitta@gmail.com>
2024-10-17 14:08:17 +0200

    The main function pulls a cat picture from TheCatAPI and displays it on screen

--- src/main.py
+++ src/main.py
@@ -1,39 +1,49 @@
 #!user/bin/env python3
 
+from PIL import Image
 import requests
 import yaml
 from pathlib import Path
 
-TEMPLATE_URL = "https://graph.instagram.com/"
+TEMPLATE_URL = "https://api.thecatapi.com/v1/images/search"
 
-TAG_ID_URL = TEMPLATE_URL + "ig_hashtag_search?user_id=<USER_ID>&q=cats"
+TEMPLATE_QUERY_URL = "?size={size}&mime_types={mime_types}&limit={limit}"
 
-TOP_MEDIA_URL = TEMPLATE_URL + "<TAG_ID>/top_media?user_id=<USER_ID>"
-
-def get_tag_id(user_id):
-    URL = TAG_ID_URL.replace("<USER_ID>", str(user_id))
-    r = requests.get(URL)
-    data = r.json()
-    return data["id"]
-
-def get_top_media_id(tag_id, user_id):
-    URL = TOP_MEDIA_URL.replace("<USER_ID>", str(user_id)).replace("<TAG_ID>", str(tag_id))
-    r = requests.get(URL)
-    data = r.json()
-    return data
-
-def main():
+def set_config():
     path = Path("../config/config.yml")
     with open(path, "r") as config_file:
         config = yaml.safe_load(config_file)
 
     creadentials = config["credentials"]
-    user_id = creadentials["user_id"]
-    tag_id = get_tag_id(user_id)
+    api_key = creadentials["api_key"]
+    return { 'x-api-key' : api_key }
 
-    data = get_top_media_id(tag_id, user_id)
-    print(data)
+def set_query_url(size, mime_types, limit):
+    temp_url_1 = TEMPLATE_QUERY_URL.replace("{size}", size)
+    temp_url_2 = temp_url_1.replace("{mime_types}", mime_types)
+    temp_url_3 = temp_url_2.replace("{limit}", str(limit))
+    return temp_url_3
 
+def get_search(headers, size="med", mime_types="jpg", limit=1):
+    query_url = set_query_url(size, mime_types, limit)
+    URL = TEMPLATE_URL + query_url
+    r = requests.get(URL, headers=headers)
+    data = r.json()
+    return data
+
+def get_image(url):
+    image = Image.open(requests.get(url, stream=True).raw)
+    image.show()
+
+def main():
+    headers = set_config()
+
+    data = get_search(headers)
+
+    image_url = data[0]["url"]
+    get_image(image_url)
 
 if __name__ == "__main__":
+    print("Starting")
     main()
+    print("Operation completed")
